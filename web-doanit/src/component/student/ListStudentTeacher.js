860899307dfd223eed371b9debc97462be43d562
TechCare <hoclephuoc1998@gmail.com>
2023-12-12 06:51:31 +0700

    hoc css

--- web-doanit/src/component/student/ListStudentTeacher.js
+++ web-doanit/src/component/student/ListStudentTeacher.js
@@ -7,7 +7,7 @@ import {NavLink} from "react-router-dom";
 import {storage} from "../../config/firebaseConfig";
 import {PaginationNav} from "./PaginationNav";
 import {LazyLoadImage} from "react-lazy-load-image-component";
-import 'bootstrap/dist/css/bootstrap.min.css';
+
 export const ListStudentTeacher = () => {
     const [students, setStudents] = useState([]);
     const [grades, setGrades] = useState([]);
@@ -75,95 +75,79 @@ export const ListStudentTeacher = () => {
         setSearchKey(searchKeyTmp);
     };
     return (
-        <div className="customCSS">
-            <div className="container">
-                <h2 className="mt-4 mb-4" style={{
-                    textAlign: 'center',
-                    backgroundColor: '#87AA74',
-                    borderRadius: '4px',
-                    height: '50px',
-                    alignItems: 'center',
-                    color: 'white',
-                    paddingTop: '6px'
-                }}>Danh sách sinh viên</h2>
-                <div className="container-fluid">
-                    <div className="row">
-                        <div className="col-8">
-                            <NavLink to={"/create-student"} className="btn btn-outline-success rounded-pill " style={{ display: 'flex', justifyContent: 'flex-start', width: '33%' }}>
-                                <span style={{ margin: 'auto' }}>Thêm sinh viên</span>
-                            </NavLink>
+        <div className="protect">
+        <div className="container">
+        <h2 className="mt-4 mb-4">Danh sách sinh viên</h2>
+        <div className="container-fluid">
+            <div className="row">
+                <div className="col-8">
+                    <NavLink to={"/create-student"} className="btn btn-outline-success rounded-pill ">
+                        Thêm sinh viên
+                    </NavLink>
+                </div>
+                <div className="col-4">
+                    <div className="input-group mb-3 rounded-pill border p-2">
+                        <input type="text" className="form-control border-0"
+                               placeholder="Tìm kiếm tên hoặc mã sinh viên "
+                               aria-label="Tìm kiếm" aria-describedby="button-addon2"
+                               value={searchKeyTmp}
+                               onChange={(e) => setSearchKeyTmp(e.target.value)}
+                               maxLength={30}
+                        />
+                        <button className="btn btn-outline-secondary border-0  btn-hover-none rounded-circle"
+                                type="button" id="button-addon2"
+                                onClick={handleSearch}
+                        ><i className="bi bi-search"></i></button>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div className="row">
+            {students.length === 0 ? <h1 className="text-center">Dữ liệu không tồn tại</h1> : <>
+                {students.map((s, index) => (<div className="col-md-3 mb-4" key={index}>
+                    <div className="card">
+                        <LazyLoadImage
+                            effect="blur" src={avatarUrls[index]} className="card-img-top img-fluid"
+                            alt={`Sinh viên ${s.index}`} width="100%"
+                        />
+                        <div className="card-body">
+                            <h5 className="card-title student-name">{s.name}</h5>
+                            <p className="card-text"><b> <i className="bi bi-code-square"></i> Mã sinh
+                                viên:</b> {"MSV".concat(s.studentId.toString().padStart(4, "0"))}</p>
+                            <p className="card-text"><b><i className="bi bi-window-sidebar"></i> Lớp:
+                            </b>{grades.find((g) => String(g.gradeId) === String(s.grade.gradeId))?.name}
+                            </p>
+                            <p className="card-text"><b><i
+                                className="bi bi-envelope"></i> Email:</b> {s.email}</p>
+                            <p className="card-text"><b><i className="bi bi-phone"></i> Điện thoại:
+                            </b> {s.phone}
+                            </p>
+                            <p className="card-text">
+                                <b>
+                                    <img src={avatarTeacherUrl}
+                                         className="bi bi-facebook rounded-circle p-1 img-teacher"
+                                         alt="Facebook"
+                                    />
+                                    GV:</b>
+                                {teacher.name}
+                            </p>
                         </div>
-                        <div className="col-4">
-                            <div className="input-group mb-3 rounded-pill border p-2">
-                                <input type="text" className="form-control border-0"
-                                       placeholder="Tìm kiếm tên hoặc mã sinh viên "
-                                       aria-label="Tìm kiếm" aria-describedby="button-addon2"
-                                       value={searchKeyTmp}
-                                       onChange={(e) => setSearchKeyTmp(e.target.value)}
-                                       maxLength={30}
-                                />
-                                <button className="btn btn-outline-secondary border-0  btn-hover-none rounded-circle"
-                                        type="button" id="button-addon2"
-                                        onClick={handleSearch}
-                                ><i className="bi bi-search"></i></button>
+                        <div className="card-footer">
+                            <div style={{float: 'right'}}>
+                                <NavLink to={`/edit/${s.studentId}`}
+                                         className="btn btn-success rounded-circle"><i
+                                    className="bi bi-pencil"></i>
+                                </NavLink>
                             </div>
                         </div>
                     </div>
-                </div>
-                <div className="row">
-                    {students.length === 0 ? <h1 className="text-center">Dữ liệu không tồn tại</h1> : <>
-                        {students.map((s, index) => (<div className="col-md-3 mb-4" key={index}>
-                            <div className="card" style={{ boxShadow: '0 4px 8px 0 rgba(0, 0, 0, 0.2)', transition: '0.3s' }}>
-                                <LazyLoadImage
-                                    effect="blur" src={avatarUrls[index]} className="card-img-top img-fluid"
-                                    style={{
-                                        objectFit: 'cover',
-                                        height: 'auto',
-                                        textAlign: 'center',
-                                        width: '100%'
-                                    }}
-                                    alt={`Sinh viên ${s.index}`} width="100%"
-                                />
-                                <div className="card-body">
-                                    <h5 className="card-title " style={{textAlign: "center",color: '#87AA74', whiteSpace: 'nowrap', overflow: 'hidden', textOverflow: 'ellipsis'}} >{s.name}</h5>
-                                    <p className="card-text"style={{ whiteSpace: 'nowrap', overflow: 'hidden', textOverflow: 'ellipsis',textAlign: 'left' }}><b> <i className="bi bi-code-square"></i> Mã sinh
-                                        viên:</b> {"MSV".concat(s.studentId.toString().padStart(4, "0"))}</p>
-                                    <p className="card-text"style={{ whiteSpace: 'nowrap', overflow: 'hidden', textOverflow: 'ellipsis',textAlign: 'left' }}><b><i className="bi bi-window-sidebar"></i> Lớp:
-                                    </b>{grades.find((g) => String(g.gradeId) === String(s.grade.gradeId))?.name}
-                                    </p>
-                                    <p className="card-text"style={{ whiteSpace: 'nowrap', overflow: 'hidden', textOverflow: 'ellipsis',textAlign: 'left' }}><b><i
-                                        className="bi bi-envelope"></i> Email:</b> {s.email}</p>
-                                    <p className="card-text"style={{ whiteSpace: 'nowrap', overflow: 'hidden', textOverflow: 'ellipsis',textAlign: 'left' }}><b><i className="bi bi-phone"></i> Điện thoại:
-                                    </b> {s.phone}
-                                    </p>
-                                    <p className="card-text"style={{ whiteSpace: 'nowrap', overflow: 'hidden', textOverflow: 'ellipsis' }}>
-                                        <b>
-                                            <img src={avatarTeacherUrl}
-                                                 className="bi bi-facebook rounded-circle p-1 img-teacher"
-                                                 style={{width: '40px',
-                                                     height: '40px}'}}
-                                                 alt="Facebook"
-                                            />
-                                            GV:</b>
-                                        {teacher.name}
-                                    </p>
-                                </div>
-                                <div className="card-footer">
-                                    <div style={{float: 'right'}}>
-                                        <NavLink to={`/edit-student/${s.studentId}`}
-                                                 className="btn btn-success rounded-circle"><i
-                                            className="bi bi-pencil"></i>
-                                        </NavLink>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>))}
-                        <PaginationNav pageNumber={pageNumber}
-                                       totalPages={totalPages}
-                                       setPageNumber={setPageNumber}
-                        />
-                    </>}
-                </div>
-            </div>
+                </div>))}
+                <PaginationNav pageNumber={pageNumber}
+                               totalPages={totalPages}
+                               setPageNumber={setPageNumber}
+                />
+            </>}
+        </div>
+    </div>
         </div>)
 }
