03b493c717106911450780e21fd9545a04534bcd
nghia20480 <nghia20480@gmail.com>
2023-12-13 00:48:54 +0700

    NghiaUpCode

--- web-doanit/src/component/ListGroupAccount/ListGroupAccount.js
+++ web-doanit/src/component/ListGroupAccount/ListGroupAccount.js
@@ -5,7 +5,9 @@ import Modal from 'react-bootstrap/Modal';
 import Form from 'react-bootstrap/Form';
 import './css.css'
 
+
 import {PaginationNav} from "./PaginationNav";
+import {toast} from "react-toastify";
 const ListGroupAccount = () => {
     const [groups, setGroup] = useState([]);
     const [pageNumber, setPageNumber] = useState(0);
@@ -16,11 +18,11 @@ const ListGroupAccount = () => {
     const [groupAcceptId,setGroupAcceptId]=useState("");
     const [groupDeleteId,setGroupDeleteId]=useState("");
     const [listStudentDeleteGroup,setListStudentDeleteGroup]=useState("");
-    const [showDelete,setShowDelete]=useState('');
-    const [showCreateDeadline,setShowCreateDeadline]=useState("");
+    const [showDelete,setShowDelete]=useState(false);
+    const [showCreateDeadline,setShowCreateDeadline]=useState(false);
     const [groupCreateDayId,setGroupCreateDayId]=useState("");
     const [deadline,setDeadline]=useState("");
-    const [isValidDeadline, setIsValidDeadline] = useState(true);
+    const [isValidDeadline, setIsValidDeadline] = useState(false);
     const [deadlineError, setDeadlineError] = useState("");
     const [nameGroup,setNameGroup]=useState("");
 
@@ -41,13 +43,13 @@ const ListGroupAccount = () => {
         setShowCreateDeadline(true)
     };
     const handleCloseCreateDeadline=()=>
-    {setShowCreateDeadline(false);
-        setDeadlineError("");}
+    {setShowCreateDeadline(false);}
     const handleDeadlineChange = (e) => {
+        setIsValidDeadline(false)
         const selectedDate = new Date(e.target.value);
         const currentDate = new Date();
         // Kiểm tra nếu ngày nhập vào lớn hơn ngày hiện tại
-        const isValid = selectedDate > currentDate;
+        const isValid = (selectedDate > currentDate);
         if (isValid) {
             setDeadline(e.target.value);
             setDeadlineError("");
@@ -73,6 +75,7 @@ const ListGroupAccount = () => {
         fetchApi()
     }, [pageNumber,searchKey,groups]) // Thực hiện một lần sau khi component được render
 
+
     const handleSearch = () => {
         setSearchKey(searchKeyTmp);
     };
@@ -80,6 +83,8 @@ const ListGroupAccount = () => {
         try {
             GroupService.acceptGroup(id)
             handleCloseAccept()
+            toast('🦄 Duyệt nhóm thành công!!!!');
+
         } catch (e){
             console.log(e);
         }
@@ -88,7 +93,7 @@ const ListGroupAccount = () => {
         try {
             GroupService.deleteGroup(id,listStudent)
             handleCloseDelete()
-            console.log(listStudentDeleteGroup)
+            toast('🦄 Xóa nhóm thành công!!!!');
         } catch (e){
             console.log(e);
         }
@@ -96,11 +101,14 @@ const ListGroupAccount = () => {
     const handleCreateDeadLine=(id,deadline)=>{
         try {
             GroupService.createDeadLine(id,deadline)
-
+            console.log(deadline)
+            toast('🦄 Tạo deadline thành công!!!!');
             handleCloseCreateDeadline()
+            setIsValidDeadline(false);
 
         } catch (e){
             console.log(e);
+
         }
     }
     return (
@@ -149,6 +157,7 @@ const ListGroupAccount = () => {
                                 <Form.Control
                                     type="datetime-local"
                                     onChange={handleDeadlineChange}
+
                                     autoFocus
                                 />
 
@@ -180,31 +189,10 @@ const ListGroupAccount = () => {
                       crossOrigin="anonymous"></link>
                 <link rel="stylesheet"
                       href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.3.0/font/bootstrap-icons.css"></link>
+                <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"></link>
             </head>
             <body>
             <div className="header">
-                {/* <nav className="navbar navbar-expand-lg navbar-light bg-light">
-                    <button className="navbar-toggler" type="button" data-toggle="collapse"
-                            data-target="#navbarTogglerDemo03" aria-controls="navbarTogglerDemo03" aria-expanded="false"
-                            aria-label="Toggle navigation">
-                        <span className="navbar-toggler-icon"></span>
-                    </button>
-                    <a className="navbar-brand" href="#">Navbar</a>
-
-                    <div className="collapse navbar-collapse" id="navbarTogglerDemo03">
-                        <ul className="navbar-nav mr-auto mt-2 mt-lg-0">
-                            <li className="nav-item active">
-                                <a className="nav-link" href="#">Home <span className="sr-only">(current)</span></a>
-                            </li>
-                            <li className="nav-item">
-                                <a className="nav-link" href="#">Link</a>
-                            </li>
-                            <li className="nav-item">
-                                <a className="nav-link disabled" href="#">Disabled</a>
-                            </li>
-                        </ul>
-                    </div>
-                </nav>*/}
             </div>
             <div className="container containerNghia">
                 <div className="col-12">
@@ -219,10 +207,10 @@ const ListGroupAccount = () => {
                                        onChange={(e) => setSearchKeyTmp(e.target.value)}
                                        maxLength={30}
                                 />
-                                <button className="btn btn-outline-secondary border-0  btn-hover-none rounded-circle btnSearchNghiaNV"
-                                        type="button" id="button-addon2 btnSearchNghia"
+                                <button className="btn btn-outline-secondary border-0  btn-hover-none rounded-circle"
+                                        type="button" id="button-addon2"
                                         onClick={handleSearch}
-                                ><i className="bi bi-search"></i></button>
+                                ><i className="bi bi-search-heart"></i></button>
                             </div>
                         </div>
                     </div>
@@ -254,13 +242,11 @@ const ListGroupAccount = () => {
                                             <td className='tdNghia'>{s.studentList.length}</td>
                                             <td className='tdNghia'>{s.date}</td>
                                             <td className='tdNghia'>
-                                                <button type="button" className="btn btn-outline-success" data-bs-toggle="modal"
-                                                        data-bs-target="#exampleModal-1" disabled={s.status===true} onClick={event =>  handleShowAccept(s.groupAccountId,s.name)} >Duyệt
+                                                <button type="button" className="btn btn-outline-success"  disabled={s.status===true} onClick={event =>  handleShowAccept(s.groupAccountId,s.name)} >Duyệt
                                                 </button>
                                             </td>
                                             <td className="btndelete no-border btndeleteNghia">
-                                                <button type="button" className="btn btn-outline-danger" data-bs-toggle="modal"
-                                                        data-bs-target="#exampleModal-2" onClick={event =>  handleShowDelete(s.groupAccountId,s.studentList.map(student => student.studentId),s.name)}>Xóa
+                                                <button type="button" className="btn btn-outline-danger"  onClick={event =>  handleShowDelete(s.groupAccountId,s.studentList.map(student => student.studentId),s.name)}>Xóa
                                                 </button>
                                             </td>
                                             <td className="btnmember no-border btnmemberNghia">
